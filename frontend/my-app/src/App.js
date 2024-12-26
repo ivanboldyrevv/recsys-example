447f01d3150219dc1091d21fc0d7736dc6534de2
ivan <1ibah1ivan1@gmail.com>
2024-12-27 01:22:53 +0400

    добавлена нумерация страниц

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,53 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import PaginatedItems from './ItemPaginate';
 
-const handleSubmit = async () => {
-  try {
-    const response = await fetch('http://localhost:8000/recommendation/personal', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ uid: 'test-uid1', item_sequence: [{ iid: '22138', description: 'BAKING SET 9 PIECE RETROSPOT'}]})
-    });
-    
-    if (!response.ok) {
-      throw new Error(`Network response was not ok (${response.status})`);
-    }
-    
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.error('There has been a problem with your fetch operation:', error);
-  }
-};
+function App() {
 
-function MyButton() {
-  handleSubmit();
   return (
-    <button>I'm a button</button>
-  );
-}
-
-export default function MyApp() {
-  return (
-    <div>
-      <h1>Welcome to my app</h1>
-      <MyButton />
+    <div className="App">
+      <PaginatedItems itemsPerPage={10} />
+      <hr></hr>
+      <h1>Рекомендации</h1>
     </div>
   );
 }
-// 
-// {
-  // "uid": "t-1-e-3-s-4-t",
-  // "item_sequence": [
-    // {
-      // "iid": "22138",
-      // "description": "BAKING SET 9 PIECE RETROSPOT"
-    // },
-    // {
-      // "iid": "23254",
-      // "description": "CHILDRENS CUTLERY DOLLY GIRL"
-    // }
-  // ]
-// }
+
+export default App;
