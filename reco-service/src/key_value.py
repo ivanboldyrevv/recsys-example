8b80e9c64b036e9d6cdf45d6ffc23c6618d52b1d
ivan <1ibah1ivan1@gmail.com>
2025-01-28 23:36:39 +0400

    refactoring: refactor abstract kv and redis class

--- reco-service/src/key_value.py
+++ reco-service/src/key_value.py
@@ -1,32 +1,46 @@
+from typing import Dict, List, Any
 from abc import ABC, abstractmethod
 import redis
 
 
 class Storage(ABC):
+    """
+        An interface that parent key-value database
+        classes must implement.
+    """
 
     @abstractmethod
-    def set(self, key, value):
+    def set(self, key: str, value: List[Any]) -> None:
         pass
 
     @abstractmethod
-    def get(self, key):
+    def get(self, key: str) -> None:
         pass
 
     @abstractmethod
-    def delete(self, key):
+    def delete(self, key: str) -> None:
         pass
 
 
 class RedisClient(Storage):
-    def __init__(self, host, port, db):
-        self.client = redis.Redis(host=host, port=port, db=db)
+    def __init__(self, config: Dict[str, str]) -> None:
+        """
+            redis wrapper.
 
-    def set(self, key, value):
-        return self.client.lpush(key, *value)
+            config (* indicates a required field):
+                host*: str = redis host
+                port*: int = redis port
+                and other unecessary redis args
+        """
+        self.r = redis.Redis(**config)
 
-    def get(self, key):
-        """fetch all items"""
-        return self.client.lrange(key, 0, -1)
+    def set(self, key: str, value: List[Any]) -> int:
+        return self.r.lpush(key, *value)
 
-    def delete(self, key):
-        return self.client.delete(key)
+    def get(self, key: str) -> List[str]:
+        values = self.r.lrange(key, 0, -1)
+        decoded = [i.decode() for i in values[::-1]]
+        return decoded
+
+    def delete(self, key: str) -> int:
+        return self.r.delete(key)
